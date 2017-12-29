59d335a6b563b4089ec3e4287cd59f902b345ebb
alexbakharew <alex.bakharew@gmail.com>
2017-12-29 16:42:31 +0300

    still working

--- 4/laba4.c
+++ 4/laba4.c
@@ -3,17 +3,18 @@
 #include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #define PAGE_SIZE 4096
 #define PATTERN_SIZE 128
+#define LINE_ERROR -101
 
 int fd;
 int page_count = 0;
 char* page = NULL;
-
-void wc(int fd) // amount of lines and chars in whole file
+void wc() // amount of lines and chars in whole file
 {
 	int c_count = 0;
 	int l_count = 0;
@@ -31,6 +32,7 @@ void wc(int fd) // amount of lines and chars in whole file
 void change_file()
 {
 	printf("Enter the name of needed file\n");
+	char file_name[128];
 	scanf("%s", file_name);
 	int temp_fd = open(file_name, O_RDWR | O_APPEND);
 	if(temp_fd > 0)
@@ -61,18 +63,16 @@ void load_page(size_t n)
 	if(page == NULL) perror("Erorr while mapping memory\n");
 	return;
 }
-size_t find_line(size_t n) // n - num of line
+int find_line(size_t n) // n - num of line
 {
 	int curr_n = 0;
-	char ch;
 	size_t i = 0;
 	load_page(0);
 	while(curr_n != n)
 	{
 		if(page == NULL)// we read all file
 		{
-			printf("No such line!\n");
-			return;
+			return LINE_ERROR;
 		}
 		if(page[i] == '\0')// end of page
 		{
@@ -90,7 +90,12 @@ size_t find_line(size_t n) // n - num of line
 }
 void read_line(size_t n) // 1
 {
-	size_t i = find_line(n);
+	int i = find_line(n);
+	if(i == LINE_ERROR) 
+	{
+		printf("No such line!\n");
+		return;
+	}
 	while(page[i] != '\n' || page[i] != '\0')
 	{
 		putchar(page[i]);
@@ -102,13 +107,16 @@ void read_line(size_t n) // 1
 
 void find_pattern(char* pattern, size_t n) // 2
 {
-	int curr_n = 0;
-	char ch;
-	size_t i = find_line(n);
+	int i = find_line(n);
+	if(i == LINE_ERROR) 
+	{
+		printf("No such line!\n");
+		return;
+	}
 	int j = 0;
 	while(page[i] != '\n') // until end of line
 	{
-		if(pattern[j] == '\n' || pattern[j] == '\0' || j == PATTREN_SIZE)
+		if(pattern[j] == '\n' || pattern[j] == '\0' || j == PATTERN_SIZE)
 		{
 			printf("Pattern was found in this line\n");
 			return;
@@ -135,7 +143,12 @@ void find_pattern(char* pattern, size_t n) // 2
 
 void overwrite(size_t n, char* str) // 3 n - num of line
 {
-	size_t i = find_line(n);
+	int i = find_line(n);
+	if(i == LINE_ERROR) 
+	{
+		printf("No such line!\n");
+		return;
+	}
 	size_t j = 0;
 	while(str[j] != '\n' || str[j] != '\0')
 	{
@@ -147,12 +160,51 @@ void overwrite(size_t n, char* str) // 3 n - num of line
 
 bool is_opened() // 4
 {
-
+	return false;
 }
 
-void actions(char* command) 
+void actions(char* command, int arg_c, char* arg_v) 
 {
-	if(strcmp(command, ))
+	if(strcmp(command, "cf") == 0) change_file();
+	else if(strcmp(command, "wc") == 0) wc();
+	else if(strcmp(command, "rl") == 0) 
+	{
+		size_t n;
+		if(arg_c == 2) n = (size_t)arg_v[2];
+		else scanf("%lu", &n);
+		read_line(n);
+	}
+	else if(strcmp(command, "fp") == 0)
+	{
+		if(arg_c == 3)
+		{
+			find_pattern(arg_v[2], (size_t)arg_v[3]);
+		}
+		else
+		{
+			size_t n;
+			char pattern[PATTERN_SIZE];
+			scanf("%lu", &n);
+			scanf("%s", pattern);
+			find_pattern(pattern, n);
+		}
+	}
+	else if(strcmp(command,"ow") == 0)
+	{
+		if(arg_c == 3)
+		{
+			overwrite((size_t)arg_v[2], arg_v[3]);
+		}
+		else
+		{
+			size_t n;
+			char text[PATTERN_SIZE];
+			scanf("%lu", &n);
+			scanf("%s", text);
+			overwrite(n, text);
+		}
+	}
+	else if(strcmp(command,"io") == 0){}
 }
 
 void info()
@@ -161,17 +213,17 @@ void info()
 	printf("cf - change file\n");
 	printf("wc - count of strings and characters\n");
 	printf("rl 'n' - read line n\n");
-	printf("fp 'n' pattern' - find pattren in line n\n");
+	printf("fp 'pattern' 'n' - find pattern in line n\n");
 	printf("ow 'n' 'text' - overwrite line n to text\n");
 	printf("io - is current file opened in other programms?\n");
 	printf("quit - exit from text editor\n");
 	return;
 }
-int main(int argc, char* argv[])
+int main()
 {
 	if(argc == 3) //non-interactive mode
 	{
-		int fd = open(argv[1], O_RDWR | O_APPEND);
+		fd = open(argv[1], O_RDWR | O_APPEND);
 		char* command = argv[2];
 		actions(command);
 		return 0;
@@ -184,7 +236,7 @@ int main(int argc, char* argv[])
 		printf("What you want to do?\n For help, hit '?'\n");
 		scanf("%s", command);
 		if(strcmp("quit", command) == 0) break;
-		actions(command);
+		actions(command,argc, argv);
 	}
 	quit(fd);
 	return 0;
