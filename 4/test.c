b2f4707a3067c5f1ea2595faf016a933327fdf8d
alex <alex.bakharew@gmail.com>
2017-12-30 00:56:40 +0300

    some functions work fine!

--- 4/test.c
+++ 4/test.c
@@ -11,15 +11,16 @@ int main()
 {
 	printf("%d\n", PAGE_SIZE);
     int fd;
-    fd = open("tmp.txt",O_RDWR | O_APPEND);
+    fd = open("test2.txt",O_RDWR | O_APPEND);
 	printf("%d\n", fd);
-	char* ptr1 = (char*) mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 0);
-	if(ptr1 == MAP_FAILED)
+	char* ptr1 = NULL;
+	ptr1 = (char*) mmap(0, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 4096);
+	if(ptr1 == MAP_FAILED || ptr1 == NULL)
 	{
 		perror("Erorr while mapping memory\n");
 		return -2;
 	}
-    ptr1[PAGE_SIZE + 1] = '\0';
+    ptr1[PAGE_SIZE - 1] = '\0';
 	printf("%s\n", ptr1);
     return 0;
 }
