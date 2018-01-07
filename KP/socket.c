262d54d60be0ebd5d632ad72efd61d5fe937a288
alexbakharew <alex.bakharew@gmail.com>
2018-01-07 15:07:08 +0300

    KP

--- KP/socket.c
+++ KP/socket.c
@@ -1,8 +1,8 @@
-//#include <sys/types.h>
-//#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdio.h>
-//#include <sys/un.h>
-//#include <unistd.h>
+#include <sys/un.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 /*typedef struct sockaddr
@@ -15,21 +15,23 @@ int main()
     int sock1;
     int sock2;
 
-    //sock1 = socket(AF_UNIX, SOCK_STREAM, 0);
-    //if(sock1 == -1) printf("Error while creating socket!\n");
+    sock1 = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(sock1 == -1) printf("Error while creating socket!\n");
 
-    //sock2 = socket(AF_UNIX, SOCK_STREAM, 0);
-    //if(sock2 == -1) printf("Error while creating socket!\n");
+    sock2 = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(sock2 == -1) printf("Error while creating socket!\n");
 
-    //struct sockaddr_un address;
-    //address.sun_family = AF_UNIX;
-    //strcpy(address.sun_path,"addr1");
+    struct sockaddr_un address;
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, "0123456789123");
 
-    //if(bind(sock1, (struct sockaddr*) &address, sizeof(address)) == -1) printf("Error while binding 1!\n");
-    //if(bind(sock2, (struct sockaddr*)&address, sizeof(address)) == -1) printf("Error while binding!\n");
+    if(bind(sock1, (struct sockaddr*) &address, sizeof(address)) == -1) printf("Error while binding 1!\n");
+    //if(bind(sock2, (struct sockaddr*)&address, sizeof(address)) == -1) printf("Error while binding 2!\n");
     
-    //close(sock1);
-    //close(sock2);
+    unlink("0123456789123");
+    //unlink("0123456789123");    
+    close(sock1);
+    close(sock2);
     printf("OK\n");
     return 0;
 }
